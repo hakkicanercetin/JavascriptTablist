a428e363584a58dec086f4bf251bcfbcdf6e61c1
Hakkıcan ERÇETİN <38086532+hakkicanercetin@users.noreply.github.com>
2023-11-12 22:35:05 +0300

    Add files via upload

--- Javascript-Tablist/main.js
+++ Javascript-Tablist/main.js
@@ -1,26 +1,33 @@
 let cities = document.getElementsByClassName("cities");
-        for(let i=0;i<cities.length;i++)
+let btn = document.getElementsByClassName("btn");
+for(let i=0;i<btn.length;i++)
+{
+    btn[i].style.fontSize = "1.5rem";
+    btn[i].style.border = "none";
+    btn[i].style.padding = "10px";
+}
+for(let i=0;i<cities.length;i++)
+{
+    cities[i].style.display = "none";
+    cities[i].style.borderTop = "1px solid grey";
+    cities[i].style.padding = "0 1rem";
+}
+function cityInfo(evt,cityName)
+{
+    let buttons = document.querySelectorAll("button")
+    for(let i=0;i<buttons.length;i++)
+    {
+        if(buttons[i].classList.length>1)
         {
-            cities[i].style.display = "none";
-            cities[i].style.borderTop = "1px solid grey";
-            cities[i].style.padding = "0 1rem";
+            buttons[i].classList.remove("active") 
         }
-        function cityInfo(evt,cityName)
-        {
-            let buttons = document.querySelectorAll("button")
-            for(let i=0;i<buttons.length;i++)
-            {
-                if(buttons[i].classList.length>1)
-                {
-                    buttons[i].classList.remove("active") 
-                }
-            }
-            evt.currentTarget.classList.add("active")
-            let cities = document.getElementsByClassName("cities");
-            for(let i=0;i<cities.length;i++)
-            {
-                cities[i].style.display = "none";
-            }
-        document.getElementById(cityName).style.display="block"
     }
-    document.getElementById("defaultOpen").click();
+    evt.currentTarget.classList.add("active")
+    let cities = document.getElementsByClassName("cities");
+    for(let i=0;i<cities.length;i++)
+    {
+        cities[i].style.display = "none";
+    }
+document.getElementById(cityName).style.display="block"
+}
+document.getElementById("defaultOpen").click();
